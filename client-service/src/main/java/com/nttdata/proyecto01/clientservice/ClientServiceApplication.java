dc18aea855b4c8b4f73e2cd03606eb5acc124770
Reynaldo Cavero Rosales <rcaveror@emeal.nttdata.com>
2022-06-19 11:11:06 -0500

    eureka

--- client-service/src/main/java/com/nttdata/proyecto01/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/nttdata/proyecto01/clientservice/ClientServiceApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.proyecto01.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
